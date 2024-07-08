2099439f56e462c85798c42f3b0756664eae1390
YiHyeongRae <dev.yihr@gmail.com>
2024-07-08 14:23:35 +0900

    feat: remove base in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "#", replacement: "/src" }],
