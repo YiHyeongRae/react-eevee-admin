5a3ca5109a39e40b89105baa7bf5b61910d9da33
YiHyeongRae <dev.yihr@gmail.com>
2024-07-08 14:33:57 +0900

    feat: edit base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: [{ find: "#", replacement: "/src" }],
-  },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    base: isProduction ? "/react-eevee-admin/" : "/",
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: [{ find: "#", replacement: "/src" }],
+    },
+  };
 });
