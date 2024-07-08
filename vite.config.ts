96f9aa1f974085e556c20e4de8979b53fd08aea3
YiHyeongRae <dev.yihr@gmail.com>
2024-07-08 14:09:03 +0900

    feat: add base path in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "#", replacement: "/src" }],
