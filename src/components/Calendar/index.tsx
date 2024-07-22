b39c90bba1eb07ca06fe13073ee849f9583c8df0
YiHyeongRae <dev.yihr@gmail.com>
2024-07-23 08:01:49 +0900

    feat: edit calendar comp

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Alert } from "../Alert";
 import { CalendarTypes } from "../../data/types/components";
 
@@ -84,26 +84,17 @@ function index({
 
   const getDayOfSelected = (year: number, month: number, date: number) => {
     const monthStr = month + 1 < 10 ? `0${month}` : month;
-    const dateStr = date + 1 < 10 ? `0${date}` : date;
+    const dateStr = date < 10 ? `0${date}` : date;
     const fullDateStr = `${year}-${monthStr}-${dateStr}`;
 
     const day = new Date(fullDateStr).getDay();
 
     return { day, fullDateStr };
   };
-  useEffect(() => {
-    select.setter(outPut);
-  }, [outPut]);
 
   const [alert, setAlert] = useState(false);
   return (
-    <div className="relative flex-auto max-w-md">
-      <div className="flex items-center justify-center gap-4 mb-4">
-        <div>{date.year}</div>
-        <>/</>
-        <div>{date.month + 1}</div>
-      </div>
-
+    <>
       <Alert
         open={alert}
         id="disable"
@@ -124,137 +115,115 @@ function index({
           },
         ]}
       />
-      <div className="calendar">
-        {/* days header */}
-        <div className="flex items-center justify-between pb-1 border-b-2">
-          {_.map(weeks, (item, index) => {
-            return (
-              <p className="flex-auto text-center" key={`${item}-${index}`}>
-                {item}
-              </p>
-            );
-          })}
+      <div className="relative flex-auto max-w-md p-4">
+        <div className="flex items-center justify-center gap-4 mb-4">
+          <div>{date.year}</div>
+          <>/</>
+          <div>{date.month + 1}</div>
         </div>
 
-        <div
-          className="grid items-center grid-cols-7 gap-2 text-center"
-          style={{ minHeight: 240 }}
-          ref={dateRef}
-        >
-          {/* prev month date */}
-          {_.times(currentMonthFirstWeeksFirstDay, (item) => {
-            const prevMonthDaysLength = new Date(
-              date.year,
-              date.month,
-              0
-            ).getDate();
-            return (
-              <p
-                id={`${date.month}-${
-                  prevMonthDaysLength -
-                  (currentMonthFirstWeeksFirstDay - item - 1)
-                }`}
-                className={`h-full flex items-center justify-center box-border cursor-pointer text-base-300`}
-                key={`prev-${item}`}
-                onClick={() => {
-                  const { day, fullDateStr } = getDayOfSelected(
-                    date.year,
-                    date.month,
-                    prevMonthDaysLength -
-                      (currentMonthFirstWeeksFirstDay - item - 1)
-                  );
+        <div className="calendar">
+          {/* days header */}
+          <div className="flex items-center justify-between pb-1 border-b-2">
+            {_.map(weeks, (item, index) => {
+              return (
+                <p className="flex-auto text-center" key={`${item}-${index}`}>
+                  {item}
+                </p>
+              );
+            })}
+          </div>
 
-                  if (
-                    (past !== "" &&
-                      new Date(fullDateStr).getTime() <
-                        new Date(past).getTime()) ||
-                    (future !== "" &&
-                      new Date(fullDateStr).getTime() >
-                        new Date(future).getTime())
-                  ) {
-                    setAlert(true);
-                  } else {
-                    monthHandler("prev");
-
-                    setOutput((prev) => {
-                      return {
-                        ...prev,
-                        month: date.month - 1,
-                        date:
-                          prevMonthDaysLength -
-                          (currentMonthFirstWeeksFirstDay - item - 1),
-                        day: day,
-                        dateStr: fullDateStr,
-                      };
-                    });
-                  }
-                }}
-              >
-                {prevMonthDaysLength -
-                  (currentMonthFirstWeeksFirstDay - item - 1)}
-              </p>
-            );
-          })}
-          {/* current month date */}
-          {_.times(currentMonthLength, (item) => {
-            return (
-              <p
-                className={`h-full flex items-center justify-center box-border cursor-pointer ${
-                  today.getMonth() === date.month &&
-                  date.today.getDate() === item + 1 &&
-                  "text-primary font-bold"
-                } ${
-                  outPut.month === date.month &&
-                  outPut.date === item + 1 &&
-                  "bg-primary text-white"
-                }
-                `}
-                key={`current-${item}`}
-                id={`${date.month + 1}-${item + 1}`}
-                onClick={() => {
-                  const { day, fullDateStr } = getDayOfSelected(
-                    date.year,
-                    date.month + 1,
-                    item + 1
-                  );
-                  if (
-                    (past !== "" &&
-                      new Date(fullDateStr).getTime() <
-                        new Date(past).getTime()) ||
-                    (future !== "" &&
-                      new Date(fullDateStr).getTime() >
-                        new Date(future).getTime())
-                  ) {
-                    setAlert(true);
-                  } else {
-                    setOutput((prev) => {
-                      return {
-                        ...prev,
-                        month: date.month,
-                        date: item + 1,
-                        day: day,
-                        dateStr: fullDateStr,
-                      };
-                    });
-                  }
-                }}
-              >
-                {item + 1}
-              </p>
-            );
-          })}
-
-          {/* next month date */}
-          {7 - nextMonthFirstWeeksFirstDay !== 7 &&
-            _.times(7 - nextMonthFirstWeeksFirstDay, (item) => {
+          <div
+            className="grid items-center grid-cols-7 gap-2 text-center"
+            style={{ minHeight: 240 }}
+            ref={dateRef}
+          >
+            {/* prev month date */}
+            {_.times(currentMonthFirstWeeksFirstDay, (item) => {
+              const prevMonthDaysLength = new Date(
+                date.year,
+                date.month,
+                0
+              ).getDate();
               return (
                 <p
-                  className="box-border flex items-center justify-center h-full cursor-pointer text-base-300"
-                  key={`next-${item}`}
+                  id={`${date.month}-${
+                    prevMonthDaysLength -
+                    (currentMonthFirstWeeksFirstDay - item - 1)
+                  }`}
+                  className={`h-full flex items-center justify-center box-border cursor-pointer text-base-300`}
+                  key={`prev-${item}`}
+                  onTouchStart={(e) => e.preventDefault()}
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                  }}
                   onClick={() => {
                     const { day, fullDateStr } = getDayOfSelected(
                       date.year,
-                      date.month + 2,
+                      date.month,
+                      prevMonthDaysLength -
+                        (currentMonthFirstWeeksFirstDay - item - 1)
+                    );
+
+                    if (
+                      (past !== "" &&
+                        new Date(fullDateStr).getTime() <
+                          new Date(past).getTime()) ||
+                      (future !== "" &&
+                        new Date(fullDateStr).getTime() >
+                          new Date(future).getTime())
+                    ) {
+                      setAlert(true);
+                    } else {
+                      monthHandler("prev");
+
+                      setOutput((prev) => {
+                        return {
+                          ...prev,
+                          month: date.month - 1,
+                          date:
+                            prevMonthDaysLength -
+                            (currentMonthFirstWeeksFirstDay - item - 1),
+                          day: day,
+                          dateStr: fullDateStr,
+                        };
+                      });
+                      select.setter(fullDateStr);
+                    }
+                  }}
+                >
+                  {prevMonthDaysLength -
+                    (currentMonthFirstWeeksFirstDay - item - 1)}
+                </p>
+              );
+            })}
+            {/* current month date */}
+            {_.times(currentMonthLength, (item) => {
+              return (
+                <p
+                  className={`h-full flex items-center justify-center box-border cursor-pointer ${
+                    today.getMonth() === date.month &&
+                    date.today.getDate() === item + 1 &&
+                    "text-primary font-bold"
+                  } ${
+                    outPut.month === date.month &&
+                    outPut.date === item + 1 &&
+                    "bg-primary text-white"
+                  }
+
+             
+                `}
+                  key={`current-${item}`}
+                  id={`${date.month + 1}-${item + 1}`}
+                  onTouchStart={(e) => e.preventDefault()}
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                  }}
+                  onClick={() => {
+                    const { day, fullDateStr } = getDayOfSelected(
+                      date.year,
+                      date.month + 1,
                       item + 1
                     );
                     if (
@@ -267,17 +236,16 @@ function index({
                     ) {
                       setAlert(true);
                     } else {
-                      monthHandler("next");
-
                       setOutput((prev) => {
                         return {
                           ...prev,
-                          month: date.month + 1,
+                          month: date.month,
                           date: item + 1,
                           day: day,
                           dateStr: fullDateStr,
                         };
                       });
+                      select.setter(fullDateStr);
                     }
                   }}
                 >
@@ -285,25 +253,79 @@ function index({
                 </p>
               );
             })}
+
+            {/* next month date */}
+            {7 - nextMonthFirstWeeksFirstDay !== 7 &&
+              _.times(7 - nextMonthFirstWeeksFirstDay, (item) => {
+                return (
+                  <p
+                    className="box-border flex items-center justify-center h-full cursor-pointer text-base-300"
+                    key={`next-${item}`}
+                    onTouchStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                    }}
+                    onClick={() => {
+                      const { day, fullDateStr } = getDayOfSelected(
+                        date.year,
+                        date.month + 2,
+                        item + 1
+                      );
+                      if (
+                        (past !== "" &&
+                          new Date(fullDateStr).getTime() <
+                            new Date(past).getTime()) ||
+                        (future !== "" &&
+                          new Date(fullDateStr).getTime() >
+                            new Date(future).getTime())
+                      ) {
+                        setAlert(true);
+                      } else {
+                        monthHandler("next");
+
+                        setOutput((prev) => {
+                          return {
+                            ...prev,
+                            month: date.month + 1,
+                            date: item + 1,
+                            day: day,
+                            dateStr: fullDateStr,
+                          };
+                        });
+                        select.setter(fullDateStr);
+                      }
+                    }}
+                  >
+                    {item + 1}
+                  </p>
+                );
+              })}
+          </div>
         </div>
-      </div>
 
-      <div className="absolute top-0 left-0 flex justify-center w-full gap-32">
-        <button
-          className="btn btn-xs btn-ghost btn-primary"
-          onClick={() => monthHandler("prev")}
-        >
-          &lt;
-        </button>
+        <div className="absolute top-[1rem] left-0 flex justify-center w-full gap-32">
+          <button
+            className="btn btn-xs btn-ghost btn-primary"
+            onClick={() => monthHandler("prev")}
+          >
+            &lt;
+          </button>
 
+          <button
+            className="btn btn-xs btn-ghost btn-primary"
+            onClick={() => monthHandler("next")}
+          >
+            &gt;
+          </button>
+        </div>
         <button
-          className="btn btn-xs btn-ghost btn-primary"
-          onClick={() => monthHandler("next")}
+          className="absolute top-[1rem] right-[1rem] btn btn-xs btn-ghost"
+          onClick={() => select.setter("")}
         >
-          &gt;
+          x
         </button>
       </div>
-    </div>
+    </>
   );
 }
 
